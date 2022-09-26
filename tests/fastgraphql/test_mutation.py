0ccc20608f911eb1623939e47e0f27595e16f481
hugowschneider <hugowschneider@gmail.com>
2022-09-26 20:01:41 +0200

    Adding Mutations (#3)
    * Mutation definition
    
    * Mutations and Name conflicts

---
+++ tests/fastgraphql/test_mutation.py
@@ -0,0 +1,177 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+from fastgraphql import FastGraphQL
+from fastgraphql.schema import SelfGraphQL, GraphQLScalar
+
+
+class TestQueryRendering:
+    def test_mutation_without_parameters(self) -> None:
+        fast_graphql = FastGraphQL()
+
+        @fast_graphql.graphql_mutation()
+        def sample_mutation() -> str:
+            return ""  # pragma: no cover
+
+        expected_query_definition = """
+sample_mutation(): String!        
+        """.strip()
+
+        expected_graphql_definition = f"""
+type Mutation {
+    {expected_query_definition}
+}""".strip()
+
+        self_graphql = SelfGraphQL.introspect(sample_mutation)
+        assert self_graphql
+        assert self_graphql.as_mutation
+        assert self_graphql.as_mutation.render() == expected_query_definition
+        assert fast_graphql.render() == expected_graphql_definition
+
+    def test_mutation_simple_parameters(self) -> None:
+        fast_graphql = FastGraphQL()
+
+        @fast_graphql.graphql_mutation()
+        def sample_mutation(
+            t_int: int = fast_graphql.graphql_query_field(),
+            t_opt_int: Optional[int] = fast_graphql.graphql_query_field(),
+            t_str: str = fast_graphql.graphql_query_field(),
+            t_opt_str: Optional[str] = fast_graphql.graphql_query_field(),
+            t_float: float = fast_graphql.graphql_query_field(),
+            t_opt_float: Optional[float] = fast_graphql.graphql_query_field(),
+            t_datatime: datetime = fast_graphql.graphql_query_field(),
+            t_opt_datatime: Optional[datetime] = fast_graphql.graphql_query_field(),
+            t_boolean: bool = fast_graphql.graphql_query_field(),
+            t_opt_boolean: Optional[bool] = fast_graphql.graphql_query_field(),
+        ) -> str:
+            return ""  # pragma: no cover
+
+        expected_query_definition = """
+sample_mutation(t_int: Int!, t_opt_int: Int, t_str: String!, t_opt_str: String, t_float: Float!, t_opt_float: Float, t_datatime: Date!, t_opt_datatime: Date, t_boolean: Boolean!, t_opt_boolean: Boolean): String!        
+        """.strip()
+
+        expected_graphql_definition = f"""
+type Mutation {
+    {expected_query_definition}
+}""".strip()
+
+        self_graphql = SelfGraphQL.introspect(sample_mutation)
+        assert self_graphql
+        assert self_graphql.as_mutation
+        assert self_graphql.as_mutation.render() == expected_query_definition
+        assert fast_graphql.render() == expected_graphql_definition
+
+    def test_mutation_renamed_parameters(self) -> None:
+        fast_graphql = FastGraphQL()
+
+        @fast_graphql.graphql_mutation()
+        def sample_mutation(
+            t_int: int = fast_graphql.graphql_query_field(name="x"),
+        ) -> str:
+            return ""  # pragma: no cover
+
+        expected_query_definition = """
+sample_mutation(x: Int!): String!        
+        """.strip()
+
+        expected_graphql_definition = f"""
+type Mutation {
+    {expected_query_definition}
+}""".strip()
+
+        self_graphql = SelfGraphQL.introspect(sample_mutation)
+        assert self_graphql
+        assert self_graphql.as_mutation
+        assert self_graphql.as_mutation.render() == expected_query_definition
+        assert fast_graphql.render() == expected_graphql_definition
+
+    def test_mutation_retyped_parameters(self) -> None:
+        fast_graphql = FastGraphQL()
+
+        @fast_graphql.graphql_mutation()
+        def sample_mutation(
+            t_int: int = fast_graphql.graphql_query_field(
+                name="x", graphql_scalar=GraphQLScalar("CustomScalar")
+            )
+        ) -> str:
+            return ""  # pragma: no cover
+
+        expected_query_definition = """
+sample_mutation(x: CustomScalar!): String!        
+        """.strip()
+
+        expected_graphql_definition = f"""
+scalar CustomScalar
+
+type Mutation {
+    {expected_query_definition}
+}""".strip()
+
+        self_graphql = SelfGraphQL.introspect(sample_mutation)
+        assert self_graphql
+        assert self_graphql.as_mutation
+        assert self_graphql.as_mutation.render() == expected_query_definition
+        assert fast_graphql.render() == expected_graphql_definition
+
+    def test_mutation_complex_parameter(self) -> None:
+        fast_graphql = FastGraphQL()
+
+        class Model(BaseModel):
+            t_int: int
+
+        @fast_graphql.graphql_mutation()
+        def sample_mutation(
+            model1: Model = fast_graphql.graphql_query_field(),
+            model2: Model = fast_graphql.graphql_query_field(),
+        ) -> str:
+            return ""  # pragma: no cover
+
+        expected_query_definition = """
+sample_mutation(model1: Model!, model2: Model!): String!        
+        """.strip()
+
+        expected_graphql_definition = f"""
+input Model {{
+    t_int: Int!
+}}
+
+type Mutation {
+    {expected_query_definition}
+}""".strip()
+
+        self_graphql = SelfGraphQL.introspect(sample_mutation)
+        assert self_graphql
+        assert self_graphql.as_mutation
+        assert self_graphql.as_mutation.render() == expected_query_definition
+        assert fast_graphql.render() == expected_graphql_definition
+
+    def test_mutation_return_complex(self) -> None:
+        fast_graphql = FastGraphQL()
+
+        class Model(BaseModel):
+            t_int: int
+
+        @fast_graphql.graphql_mutation()
+        def sample_mutation() -> Model:
+            return Model()  # pragma: no cover
+
+        expected_query_definition = """
+sample_mutation(): Model!        
+        """.strip()
+
+        expected_graphql_definition = f"""
+type Model {{
+    t_int: Int!
+}}
+
+type Mutation {
+    {expected_query_definition}
+}""".strip()
+
+        self_graphql = SelfGraphQL.introspect(sample_mutation)
+        assert self_graphql
+        assert self_graphql.as_mutation
+        assert self_graphql.as_mutation.render() == expected_query_definition
+        assert fast_graphql.render() == expected_graphql_definition
